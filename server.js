cd5998aeee8637b0eb02e969b038aa93896a3ce5
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-07-01 16:15:20 -0500

    Se actualizo el server con el inicio de sesion

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import session from "express-session";
 import { userRouter } from "./api/routes/index.js";
 
 const app = express();
@@ -8,13 +8,29 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
 // Routes
 
 app.get("/", (req, res) => {
-  res.send("Bienvenido a Puppy Heaven");
+  try {
+    if(req.session.loggedin == true){
+      res.send("Bienvenido a Puppy Heaven Usuario Iniciado");
+    } else{
+      console.log("Sin inicio de sesión");
+      res.send("Bienvenido a Puppy Heaven");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error en el servidor");
+  }
 });
+
 app.use("/api", userRouter);
+
 
 const PORT = process.env.PORT || 5000;
 const USER = process.env.USER;
