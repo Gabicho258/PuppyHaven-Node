01a5991bd843e3d718c6fdac26f4bc4daa9761b5
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-07-03 17:10:03 -0500

    Se añadieron las rutas

---
+++ api/models/index.js
@@ -0,0 +1,7 @@
+import userModel from "./user.login.model.js";
+import walkerModel from "./walker.login.model.js";
+
+// import petRouter from "./pet.router.js";
+
+// export { userRouter, petRouter };
+export { userModel, walkerModel };
