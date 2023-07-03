425ad7f6b8fd208620eb33cc6c2ec8591b497c00
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-07-03 17:10:56 -0500

    Se añadieron las rutas

---
+++ api/routes/user.login.router.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+import { userController } from "../controllers/index.js";
+
+const { register, auth, storyUser, login, logout } = userController;
+
+const router = express.Router();
+const userRoutes = {
+  REGISTER: "/users/register",
+  LOGIN: "/users/login",
+  LOGOUT: "/users/logout"
+};
+
+router.get(userRoutes.REGISTER, register);
+router.post(userRoutes.REGISTER, storyUser);
+router.get(userRoutes.LOGIN, login);
+router.post(userRoutes.LOGIN, auth);
+router.get(userRoutes.LOGOUT, logout);
+
+
+
+export default router;
